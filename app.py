279d21e01b5553b06220d052486c09a4d4a837db
juarezdiegonicolas-proyectos <juarezdiegonicolas@gmail.com>
2026-08-01 21:12:21 -0300

    app.py

--- app.py
+++ app.py
@@ -6,5 +6,37 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/escritorio')
+def escritorio():
+    return render_template('escritorio.html')
+
+@app.route('/escritorio_admin')
+def escritorio_admin():
+    return render_template('escritorio_admin.html')
+
+@app.route('/escritorio_ascensores')
+def escritorio_ascensores():
+    return render_template('escritorio_ascensores.html')
+
+@app.route('/escritorio_camilleros')
+def escritorio_camilleros():
+    return render_template('escritorio_camilleros.html')
+
+@app.route('/escritorio_conmutador')
+def escritorio_conmutador():
+    return render_template('escritorio_conmutador.html')
+
+@app.route('/escritorio_dalo')
+def escritorio_dalo():
+    return render_template('escritorio_dalo.html')
+
+@app.route('/escritorio_intendencia')
+def escritorio_intendencia():
+    return render_template('escritorio_intendencia.html')
+
+@app.route('/escritorio_lavadero')
+def escritorio_lavadero():
+    return render_template('escritorio_lavadero.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
